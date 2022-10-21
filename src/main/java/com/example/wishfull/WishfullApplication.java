67ba925ddc03114899e673b5ea958ded339ca926
BasmaJawad <basm0035@stud.kea.dk>
2022-10-21 16:43:26 +0200

    packages is up

---
+++ src/main/java/com/example/wishfull/WishfullApplication.java
@@ -0,0 +1,13 @@
+package com.example.wishfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishfullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishfullApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wishfull/WishfullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wishfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishfullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
